2160fe621fe9d910811e79513e08e7f0d010b72f
geoffrey <geoffrey@renaissance.nl>
2023-01-12 11:44:22 +0100

    Fix return type getRecordTitle()
    Signed-off-by: geoffrey <geoffrey@renaissance.nl>

--- src/Resources/Pages/Concerns/HasActiveFormLocaleSelect.php
+++ src/Resources/Pages/Concerns/HasActiveFormLocaleSelect.php
@@ -23,7 +23,7 @@ trait HasActiveFormLocaleSelect
             );
     }
 
-    public function getRecordTitle(): ?string
+    public function getRecordTitle(): string
     {
         if ($this->activeFormLocale) {
             $this->record->setLocale($this->activeFormLocale);
